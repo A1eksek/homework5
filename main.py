3b9ee1f74c878bd758316d3b976fc0abe922b5ce
A1lekse <alexey20080705@gmail.com>
2024-06-27 09:47:33 +0400

    сделаное дз

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-# This is a sample Python script.
+class Homer:
+    def __init__(self, name):
+        self.name = name
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Daughter(Homer):
+    pass
+
+daughter1 = Daughter('Lisa')
+print(daughter1.name)
+
+####################################
+
+class Shape:
+    def calculate_area(self):
+        pass
+
+class Rectangle(Shape):
+    pass
+
+class Circle(Shape):
+    pass
+
+class Triangle(Shape):
+    pass
+
+#########################################
+
+class Animal:
+    def make_sound(self):
+        print('')
+
+class Dog(Animal):
+    def make_sound(self):
+        print('гав')
+
+class Cat(Animal):
+    def make_sound(self):
+        print('мяу')
+
+class Bird(Animal):
+    def make_sound(self):
+        print('чик-чирик')
+
+dog = Dog()
+cat = Cat()
+bird = Bird()
+
+dog.make_sound()
+cat.make_sound()
+bird.make_sound()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
